fff90bf0535c2ccd163fdcc89a48c15b4b32775d
arriagadakevin <arriagadakevin55@gmail.com>
2023-06-25 22:09:20 -0300

    manejo de fps

--- player.py
+++ player.py
@@ -11,6 +11,8 @@ class Player:
         self.stay_l = auxiliar.getSurfaceFromSpriteSheet(stay_l,10,1)
         self.jump_r = auxiliar.getSurfaceFromSpriteSheet(jump_r,3,1)
         self.jump_l = auxiliar.getSurfaceFromSpriteSheet(jump_l,3,1)
+        self.fallin_r = auxiliar.getSurfaceFromSpriteSheet(fall_r,2,1)
+        self.fallin_l = auxiliar.getSurfaceFromSpriteSheet(fall_l,2,1)
         self.frame = 0 
         self.live = 5 
         self.score = 0
@@ -21,15 +23,17 @@ class Player:
         self.jump_velocidad = 0
         self.jumping = jump_valor
         self.is_jump = False
+        self.is_fallin = False
         self.gravity = gravedad
         self.animation = self.stay_r
         self.animation_speed = 0.2
-        self.animation_counter = 0
         self.direccion = direccion
         self.image = self.animation[self.frame]
         self.rect = self.image.get_rect()
         self.rect.x = X
         self.rect.y = Y
+        self.tiempo_transcurrido_animacion = 0
+        self.tiempo_transcurrido_movimiento = 0 
        
         
     
@@ -51,6 +55,8 @@ class Player:
             else:
                 self.animation = self.jump_l
             self.frame = 0
+            
+        
     def stay(self):
         
         if self.direccion == "derecha":
@@ -60,32 +66,50 @@ class Player:
         self.mover_x = 0
         self.mover_y = 0
         self.frame = 0
+
+    def todos_los_movimientos(self,delta_ms):
+        self.tiempo_transcurrido_movimiento += delta_ms
+
+        if self.tiempo_transcurrido_movimiento >= 20:
+                self.tiempo_transcurrido_movimiento = 0 
+                if self.is_fallin == True:
+                    self.is_fallin = False
+                    self.frame = 0
+                    if self.direccion == "derecha":
+                        self.animation = self.fallin_r
+                    else:
+                        self.animation = self.fallin_l
+
+                if self.is_jump:
+                    self.rect.y += self.jump_velocity
+                    self.jump_velocity += GRAVEDAD
+                    self.is_fallin = True
+
+                
+                self.rect.x += self.mover_x
+                self.rect.y += self.mover_y
+                
+                if self.rect.y >= SUELO_PRINCIPAL:
+                    self.rect.y = SUELO_PRINCIPAL
+                    self.jump_velocity = 0
+                    self.is_jump = False
+
                 
 
-    def update(self,delta_ms):
+    def todas_las_animacion(self,delta_ms):
+        self.tiempo_transcurrido_animacion += delta_ms
 
-        
-        self.animation_counter += self.animation_speed
-        self.rect.y += self.gravity
-
-        if self.animation_counter >= 1:
-            self.animation_counter = 0  # Reiniciar el contador
+        if self.tiempo_transcurrido_animacion >= 40:
+            self.tiempo_transcurrido_animacion = 0 
             if (self.frame < len(self.animation) - 1):
                 self.frame += 1
             else:
                 self.frame = 0
-        if self.is_jump:
-            self.rect.y += self.jump_velocity
-            self.jump_velocity += GRAVEDAD
-
-        
-        self.rect.x += self.mover_x
-        self.rect.y += self.mover_y
-        
-        if self.rect.y >= SUELO_PRINCIPAL:
-            self.rect.y = SUELO_PRINCIPAL
-            self.jump_velocity = 0
-            self.is_jump = False
+            
+            
+    def update(self,delta_ms):
+        self.todos_los_movimientos(delta_ms)
+        self.todas_las_animacion(delta_ms)
         
 
         
